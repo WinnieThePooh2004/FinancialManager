382ae69ecc99b25a89f075df08e31eefa5110ead
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-10-27 18:59:33 +0300

    1.0.1 DTOs moved to separate library

--- FinancialManager/Frontend/DTOs/OperationTypes/OperationTypeUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.DTOs.OperationTypes
-{
-    public class OperationTypeUpdateDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = default!;
-        public string IsIncome { get; set; } = default!;
-    }
-}
