01b2f9fc08ca42ffcdcb07ff6eeb5c4d27e5aa3a
WinnieThePooh2004 <vova2004hunko@icloud.com>
2022-12-03 23:31:31 +0200

    added migrations project

---
+++ FinancialManager/Migrations/Migrations/20221203213042_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OperationTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsIncome = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OperationTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinancialOperations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    OperationTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialOperations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinancialOperations_OperationTypes_OperationTypeId",
+                        column: x => x.OperationTypeId,
+                        principalTable: "OperationTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinancialOperations_OperationTypeId",
+                table: "FinancialOperations",
+                column: "OperationTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinancialOperations");
+
+            migrationBuilder.DropTable(
+                name: "OperationTypes");
+        }
+    }
+}
---
+++ FinancialManager/Migrations/Program.cs
@@ -0,0 +1,34 @@
+﻿using FinancialManager.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+public class Program : IDesignTimeDbContextFactory<FinancialManagerContext>
+{
+    public FinancialManagerContext CreateDbContext(string[] args)
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        IConfigurationRoot configuration = configurationBuilder.Build();
+
+        var connString = configuration.GetConnectionString("FinancialManagerContext");
+
+        DbContextOptionsBuilder<FinancialManagerContext> optionsBuilder = new DbContextOptionsBuilder<FinancialManagerContext>()
+            .UseSqlServer(connString, b => b.MigrationsAssembly("Migrations"));
+
+        return new FinancialManagerContext(optionsBuilder.Options);
+    }
+
+    static void Main(string[] args)
+    {
+
+        Program p = new Program();
+
+        using (FinancialManagerContext db = p.CreateDbContext(args))
+        {
+            db.Database.Migrate();
+        }
+    }
+}
